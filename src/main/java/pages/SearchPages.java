ed92e3bb17fe3640d1664731a7d7b21341f94d84
beyzatsdln <beyzakavgacii@gmail.com>
2024-11-18 17:00:47 +0300

    initial commit2

--- src/main/java/pages/SearchPages.java
+++ src/main/java/pages/SearchPages.java
@@ -1,18 +1,16 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
-import dev.failsafe.internal.util.Assert;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SearchPages extends LoginPages {
 
-    private SelenideElement searchbox =$x("//input[@id='q']");
-    private SelenideElement searchbutton = $x("//button[@title='Ara']");
-    private SelenideElement product = $x("//li[@data-mk='Stanley' and @data-pr='1343650']//h3[@class='pn_v8']");
-    private SelenideElement followbutton =$x("//div[@id='ntf_w']//span[@class='ufo_v8 a']");
+    private final SelenideElement searchbox =$x("//input[@id='q']");
+    private final SelenideElement searchbutton = $x("//button[@title='Ara']");
+    private final SelenideElement product = $x("//li[@data-mk='Stanley' and @data-pr='1343650']//h3[@class='pn_v8']");
+    private final SelenideElement followbutton =$x("//div[@id='ntf_w']//span[@class='ufo_v8']");
 
 
     public void searchproduct(String text){
@@ -25,7 +23,7 @@ public class SearchPages extends LoginPages {
         product.shouldBe(visible).click();
     }
 
-    public void Followproduct(){
+    public void followproduct(){
         followbutton.shouldBe(visible).click();
 
     }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,32 +8,41 @@ import pages.SearchPages;
 import static com.codeborne.selenide.Selenide.open;
 
 
-public class LoginTest {
+public class LoginTest{
     @BeforeTest
     public static void setup() {
 
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-
+        open("https://www.akakce.com/");
     }
 
     @Test
 
     public void LoginTest1() {
-        open("https://www.akakce.com/");
+
 
         LoginPages loginPages = new LoginPages();
-        SearchPages searchPages = new SearchPages();
 
         loginPages.openLogin();
         loginPages.loginform("wvsxpdcujx@gumglue.app", "Aa!123456");
+    }
+
+    @Test
+
+    public void SearchTest() {
+        SearchPages searchPages = new SearchPages();
 
         searchPages.searchproduct("Stanley");
         searchPages.selectproduct();
+    }
 
-        searchPages.Followproduct();
+    @Test
 
+     public void FollowTest(){
+        SearchPages searchPages = new SearchPages();
+        searchPages.followproduct();
 
-        }
+     }
     }
 
